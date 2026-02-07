8b86ab9d8001f3f3987f78e4df869fe5e9b5369d
Aleksei Avitsuk <ky200782aav@gmail.com>
2026-02-07 19:45:06 +0100

    Update api_manager.py

--- src/main/api/classes/api_manager.py
+++ src/main/api/classes/api_manager.py
@@ -1,6 +1,8 @@
 from src.main.api.steps.admin_steps import AdminSteps
+from src.main.api.steps.build_steps import BuildSteps
 
 
 class ApiManager:
     def __init__(self, created_objects: list):
         self.admin_steps: AdminSteps = AdminSteps(created_objects)
+        self.build_steps: BuildSteps = BuildSteps(created_objects)
