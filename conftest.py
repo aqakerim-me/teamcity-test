bbe9d82b2d5fc7afb02ae65483ce028cfb86dfb2
Aleksei Avitsuk <ky200782aav@gmail.com>
2026-02-07 19:44:56 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 from src.main.api.fixtures.api_fixtures import *
 from src.main.api.fixtures.object_fixtures import *
 from src.main.api.fixtures.user_fixtures import *
+from src.main.api.fixtures.build_fixtures import *
 
 
